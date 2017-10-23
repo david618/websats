5bc101706da1e10ce7507c7851b959a82146928a
David Jennings <djennings@esri.com>
2017-10-23 11:29:17 -0500

    Coded satinfo; fix pom.xml for Glassfish

--- src/main/java/org/jennings/websats/satinfo.java
+++ src/main/java/org/jennings/websats/satinfo.java
@@ -28,19 +28,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.jennings.geotools.Circle;
-import org.jennings.geotools.GreatCircle;
+import org.jennings.geotools.GeographicCoordinate;
 import org.jennings.mvnsat.Sat;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * In Development: Output json or delimited txt with point, line(Path for +- 1/2 day, and polygon for a specified satellite.
+ * In Development: Output json or delimited txt with point, line(Path for +- 1/2
+ * day, and polygon for a specified satellite.
+ *
  * @author djennings
  */
 @WebServlet(name = "satinfo", urlPatterns = {"/satinfo"})
 public class satinfo extends HttpServlet {
 
-    private static Sats satDB = null;    
+    private static Sats satDB = null;
+
+    private GeographicCoordinate findCrossing(double x1, double y1, double x2, double y2) {
+        GeographicCoordinate pt;
+
+        double m = (y2 - y1) / (x2 - x1);
+
+        double latDT = m * (180.0 - x1) + y1;
+
+        return new GeographicCoordinate(180.0, latDT);
+
+    }    
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -58,42 +71,21 @@ public class satinfo extends HttpServlet {
                 satDB = new Sats();
             }
 
-            String strFormat = "";
             String strGeomType = "";
-            String strNums = "";
-            String strNames = "";
-            String strTime = "";
-            String strNumPoints = "";
-            boolean isNums = false;
+            String strNum = "";
 
             // Populate the parameters ignoring case
             Enumeration paramNames = request.getParameterNames();
             while (paramNames.hasMoreElements()) {
                 String paramName = (String) paramNames.nextElement();
-                if (paramName.equalsIgnoreCase("f")) {
-                    strFormat = request.getParameter(paramName);
-                } else if (paramName.equalsIgnoreCase("gt")) {
+                if (paramName.equalsIgnoreCase("gt")) {
                     strGeomType = request.getParameter(paramName);
-                } else if (paramName.equalsIgnoreCase("t")) {
-                    strTime = request.getParameter(paramName);
-                } else if (paramName.equalsIgnoreCase("npts")) {
-                    strNumPoints = request.getParameter(paramName);
-                } else if (paramName.equalsIgnoreCase("names")) {
-                    strNames = request.getParameter(paramName);
-                } else if (paramName.equalsIgnoreCase("nums")) {
-                    strNums = request.getParameter(paramName);
-                    isNums = true;
+                } else if (paramName.equalsIgnoreCase("num")) {
+                    strNum = request.getParameter(paramName);
                 }
             }
 
             long t = System.currentTimeMillis();  // Default 1 minute before current time
-            if (!strTime.equalsIgnoreCase("")) {
-                t = Long.parseLong(strTime);
-                if (t < 10000000000L) {
-                    // Assume it seconds convert to ms
-                    t = t * 1000;
-                }
-            }
 
             boolean isClockwise = true;  // Esri Geoms are clockwise
             if (!strGeomType.equalsIgnoreCase("")) {
@@ -104,41 +96,19 @@ public class satinfo extends HttpServlet {
                 }
             }
 
-            String format = "txt";
-            if (!strFormat.equalsIgnoreCase("")) {
-                format = strFormat;
-                if (strFormat.equalsIgnoreCase("geojson")) {
-                    isClockwise = false;
-                }
-            }
-
             int numPoints = 50;
-            if (!strNumPoints.equalsIgnoreCase("")) {
-                numPoints = Integer.parseInt(strNumPoints);
-                if (numPoints < 20) {
-                    numPoints = 20;
-                }
-                if (numPoints > 500) {
-                    numPoints = 500;
-                }
-            }
 
             HashSet<String> sats = new HashSet<>();
 
-            if (strNames.equalsIgnoreCase("") && strNums.equalsIgnoreCase("")) {
-                sats = satDB.getAllNums();
-            }
-
-            if (!strNums.equalsIgnoreCase("")) {
+            if (!strNum.equalsIgnoreCase("")) {
                 // Nums were specified 
-                sats = satDB.getSatsByNum(strNums);
-            } else if (!strNames.equalsIgnoreCase("")) {
-                // Names were specified
-                sats = satDB.getSatsByName(strNames);
+                sats = satDB.getSatsByNum(strNum);
+            } else {
+                // Get ISS 
+                sats = satDB.getSatsByNum("25544");
             }
 
             JSONArray results = new JSONArray();
-            String strLines = "";
 
             int i = 0;
             PrintWriter out = response.getWriter();
@@ -163,51 +133,115 @@ public class satinfo extends HttpServlet {
                     e.printStackTrace();
                 }
 
-                if (format.equalsIgnoreCase("json") || format.equalsIgnoreCase("ndjson") || format.equalsIgnoreCase("txt")) {
-
-                    JSONObject geom2 = new JSONObject();
-                    if (strGeomType.equalsIgnoreCase("geojson")) {
-                        geom2.put("coordinates", polys);
-
-                    } else {
-                        geom2.put("rings", polys.getJSONArray(0));
-                    }
-
-                    if (format.equalsIgnoreCase("json")) {
-                        result.put("name", pos.getName());
-                        result.put("num", sat);
-                        result.put("geometry", geom2);
-                        if (format.equalsIgnoreCase("json")) {
-                            results.put(result);
-                        } else {
-                            out.println(result.toString());
-                        }
-                        
-
-                    } else {
-                        // Default to delimited
-                        String strLine = pos.getName() + "|" + pos.getNum() + "|"
-                                + geom2.toString() + "";
-                        out.println(strLine);
-
-                    }
+                JSONObject geomFootprint = new JSONObject();
+                if (strGeomType.equalsIgnoreCase("geojson")) {
+                    geomFootprint.put("coordinates", polys);
 
                 } else {
-                    throw new Exception("Invalid format. Supported formats: txt,json, or ndjson");
+                    JSONArray rngs = new JSONArray();
+                    int numPolys = polys.length();
+                    int polyNum = 0;
+                    while (polyNum < numPolys) {
+                        rngs.put(polys.getJSONArray(polyNum).getJSONArray(0));
+                        polyNum++;
+                    }
+                    geomFootprint.put("rings", rngs);
                 }
 
+                long t1 = System.currentTimeMillis();
+                long tstart = t1;
+                long tduration = 7200; // 7200 seconds or 2 hours
+                pos = st.getPos(t1);
+                double lat1 = pos.GetParametricLat();
+                double lon1 = pos.GetLon();
+                JSONArray tracks = new JSONArray();
+                JSONArray track = new JSONArray();
+
+                while (t1 <= tstart + tduration * 1000) {
+                    pos = st.getPos(t1);
+                    double lat2 = pos.GetParametricLat();
+                    double lon2 = pos.GetLon();
+
+                    // If you cross the DateTime line then inject a break at 180
+                    if (Math.abs(lon2 - lon1) > 180.0) {
+                        System.out.println("");
+                        System.out.println(pos.getName());
+                        System.out.println(lon1 + "," + lat1);
+                        System.out.println("Break");
+                        System.out.println(lon2 + "," + lat2);
+                        double x1 = lon1;
+                        double x2 = lon2;
+                        if (lon1 < 0) {
+                            x1 += 360.0;
+                        }
+                        if (lon2 < 0) {
+                            x2 += 360.0;
+                        }
+                        GeographicCoordinate crossing = findCrossing(x1, lat1, x2, lat2);
+                        System.out.println(crossing);
+
+                        if (lon1 < 0) {
+                            // Insert with -180
+                            JSONArray coord = new JSONArray("[-180, " + crossing.getLat() + "]");
+                            track.put(coord);
+                        } else {
+                            // Insert with 180
+                            JSONArray coord = new JSONArray("[180, " + crossing.getLat() + "]");
+                            track.put(coord);
+                        }
+                        // Insert line and start new line
+                        tracks.put(track);
+                        track = new JSONArray();
+
+                        if (lon2 < 0) {
+                            // Start with -180
+                            JSONArray coord = new JSONArray("[-180, " + crossing.getLat() + "]");
+                            track.put(coord);
+                        } else {
+                            // Start with 180
+                            JSONArray coord = new JSONArray("[180, " + crossing.getLat() + "]");
+                            track.put(coord);
+                        }
+
+                    }
+
+                    JSONArray coord = new JSONArray("[" + pos.GetLon() + ", " + pos.GetParametricLat() + "]");
+                    track.put(coord);
+
+                    lat1 = lat2;
+                    lon1 = lon2;
+
+                    // Step 60 seconds
+                    t1 += 60 * 1000;
+                }
+                tracks.put(track);
+
+                JSONObject geomTrack = new JSONObject();
+                if (strGeomType.equalsIgnoreCase("geojson")) {
+                    geomTrack.put("coordinates", tracks);
+
+                } else {
+                    JSONArray path = new JSONArray();
+                    path.put(tracks);
+                    geomTrack.put("paths", path);
+                }
+
+                result.put("name", pos.getName());
+                result.put("num", sat);
+                result.put("timestamp", pos.GetEpoch().epochTimeMillis());
+                result.put("dtg", pos.GetEpoch());
+                result.put("lon", pos.GetLon());
+                result.put("lat", pos.GetParametricLat());
+                result.put("alt", pos.getAltitude());
+                result.put("geomftprt", geomFootprint);
+                result.put("geomtrack", geomTrack);
+
+                results.put(result);
+
             }
 
-
-            if (format.equalsIgnoreCase("json")) {
-                response.setContentType("application/json;charset=UTF-8");
-                results.write(out);
-
-            } else {
-                // Pipe Delimited
-                response.setContentType("text/plain;charset=UTF-8");
-
-            }
+            response.setContentType("application/json;charset=UTF-8");
+            results.write(out);
 
         } catch (Exception e) {
             try (PrintWriter out = response.getWriter()) {
@@ -225,7 +259,7 @@ public class satinfo extends HttpServlet {
                 out.println("</html>");
             }
         }
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
